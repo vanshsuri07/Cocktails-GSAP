9e5960121f7189954fb6e2e3c31d55cced2764c8
Vanshsuri07 <vanshsuri07@gmai.com>
2025-07-01 21:53:52 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import { ScrollTrigger, SplitText } from "gsap/all";
 import gsap from "gsap";
+import Navbar from "./components/navbar";
+import Hero from "./components/Hero";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
   return (
-    <div className="flex-center">
-      <h1 className="text-4xl font-bold text-indigo-300 text-center mt-10">
-        Mojito Cocktail Recipe
-      </h1>
-    </div>
+    <main>
+      <Navbar />
+      <Hero />
+      <div className="h-dvh bg-black"></div>
+    </main>
   );
 };
 
---
+++ src/components/Hero.jsx
@@ -0,0 +1,95 @@
+import { useGSAP } from "@gsap/react";
+import { SplitText } from "gsap/SplitText";
+import gsap from "gsap";
+
+const Hero = () => {
+  useGSAP(() => {
+    const heroSpilt = new SplitText(".title", { type: "chars, words" });
+
+    const paragraghSplit = new SplitText(".subtitle", { type: "lines" });
+
+    heroSpilt.chars.forEach((char) => char.classList.add("text-gradient"));
+
+    gsap.from(heroSpilt.chars, {
+      yPercent: 100,
+      duration: 1.8,
+      ease: "expo.out",
+      stagger: 0.05,
+    });
+
+    gsap.from(paragraghSplit.lines, {
+      opacity: 0,
+      yPercent: 100,
+      duration: 1.8,
+      ease: "expo.out",
+      stagger: 0.06,
+      delay: 1,
+    });
+
+    gsap
+      .timeline({
+        scrollTrigger: {
+          trigger: "#hero",
+          start: "top top",
+          end: "bottom top",
+          scrub: true,
+        },
+      })
+      .to(
+        ".right-leaf",
+        {
+          y: 200,
+        },
+        0
+      )
+      .to(
+        ".left-leaf",
+        {
+          y: -200,
+        },
+        0
+      );
+  }, []);
+
+  return (
+    <>
+      <section id="hero" className="noisy">
+        <h1 className="title">MOJITO</h1>
+
+        <img
+          src="/images/hero-left-leaf.png"
+          alt="left leaf"
+          className="left-leaf"
+        />
+
+        <img
+          src="/images/hero-right-leaf.png"
+          alt="right leaf"
+          className="right-leaf"
+        />
+
+        <div className="body">
+          <div className="content">
+            <div className="space-y-5 hidden md:block">
+              <p>Cool. Crisp. Classic.</p>
+              <p className="subtitle">
+                Sip the Spirit <br /> of Summer
+              </p>
+            </div>
+
+            <div className="view-cocktails">
+              <p className="subtitle">
+                Every cocktail on our menu is a blend of premium ingredients,
+                creative flair, and timeless recipes — designed to delight your
+                senses. View cocktails
+              </p>
+              <a href="#cocktails">View Cocktails</a>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Hero;
