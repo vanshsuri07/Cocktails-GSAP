716b60b75e479fb57ecda223a5b207b6ff0ce25c
Vanshsuri07 <vanshsuri07@gmai.com>
2025-07-21 19:14:18 +0530

    Updated Hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,14 @@
 import { useGSAP } from "@gsap/react";
 import { SplitText } from "gsap/SplitText";
 import gsap from "gsap";
+import { useRef } from "react";
+import { useMediaQuery } from "react-responsive";
 
 const Hero = () => {
+  const videoRef = useRef();
+
+  const isMobile = useMediaQuery({ maxWidth: 767 });
+
   useGSAP(() => {
     const heroSpilt = new SplitText(".title", { type: "chars, words" });
 
@@ -49,6 +55,24 @@ const Hero = () => {
         },
         0
       );
+    const startValue = isMobile ? "top 50%" : "center 60%";
+    const endValue = isMobile ? "top 120%" : "bottom top";
+
+    const t1 = gsap.timeline({
+      scrollTrigger: {
+        trigger: "video",
+        start: startValue,
+        end: endValue,
+        scrub: true,
+        pin: true,
+      },
+    });
+
+    videoRef.current.onloadedmetadata = () => {
+      t1.to(videoRef.current, {
+        currentTime: videoRef.current.duration,
+      });
+    };
   }, []);
 
   return (
@@ -88,6 +112,16 @@ const Hero = () => {
           </div>
         </div>
       </section>
+
+      <div className="video absolute inset-0">
+        <video
+          ref={videoRef}
+          src="/videos/output.mp4"
+          muted
+          playsInline
+          preload="auto"
+        />
+      </div>
     </>
   );
 };
