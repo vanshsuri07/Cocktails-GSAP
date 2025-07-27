d1e77aedcee04f08bb36c8ce045d1ef793702ecf
Vanshsuri07 <vanshsuri07@gmai.com>
2025-07-27 23:39:28 +0530

    implement about

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import gsap from "gsap";
 import Navbar from "./components/navbar";
 import Hero from "./components/Hero";
 import Cocktails from "./components/Cocktails";
+import About from "./components/About";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -12,6 +13,7 @@ const App = () => {
       <Navbar />
       <Hero />
       <Cocktails />
+      <About />
     </main>
   );
 };
---
+++ src/components/About.jsx
@@ -0,0 +1,104 @@
+import { useGSAP } from "@gsap/react";
+import { SplitText } from "gsap/all";
+import gsap from "gsap";
+
+const About = () => {
+  useGSAP(() => {
+    const titleSpilt = SplitText.create("#about h2", {
+      type: "words",
+    });
+
+    const scrollTimeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: "#about",
+        start: "top top",
+      },
+    });
+
+    scrollTimeline
+      .from(titleSpilt.words, {
+        opacity: 0,
+        duration: 1,
+        yPercent: 100,
+        stagger: 0.02,
+        ease: "expo.out",
+      })
+      .from(
+        ".top-grid div, .bottom-grid div",
+        {
+          opacity: 0,
+          duration: 1,
+          ease: "power1.inOut",
+          stagger: 0.04,
+        },
+        "-=0.5"
+      );
+  });
+
+  return (
+    <div id="about">
+      <div className="mb-16 md:px-0 px-5">
+        <div className="content">
+          <div className="md:col-span-8">
+            <p className="badge">Best Cocktails</p>
+            <h2>
+              Where every detail matters <span className="text-white">-</span>
+              from muddle to garnish
+            </h2>
+          </div>
+
+          <div className="sub-content">
+            <p>
+              Every cocktail we serve is a testament to our commitment to
+              quality and craftsmanship. From the moment we select the finest
+              ingredients to the final flourish of garnish, we ensure that every
+              sip is an experience to remember. Our team of expert mixologists
+              meticulously crafts each drink, blending flavors and techniques
+              that elevate your cocktail experience. Join us in celebrating the
+              art of mixology, where passion meets precision in every glass.
+            </p>
+
+            <div>
+              <p className="md:text-3xl text-xl font-bold">
+                <span>4.5</span>/5
+              </p>
+              <p className="text-sm text-white-100">
+                More than +12000 customers
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="top-grid">
+        <div className="md:col-span-3">
+          <div className="noisy" />
+          <img src="/images/abt1.png" alt="grid-img-1" />
+        </div>
+
+        <div className="md:col-span-6">
+          <div className="noisy" />
+          <img src="/images/abt2.png" alt="grid-img-2" />
+        </div>
+
+        <div className="md:col-span-3">
+          <div className="noisy" />
+          <img src="/images/abt5.png" alt="grid-img-5" />
+        </div>
+      </div>
+      <div className="bottom-grid">
+        <div className="md:col-span-8">
+          <div className="noisy" />
+          <img src="/images/abt3.png" alt="grid-img-3" />
+        </div>
+
+        <div className="md:col-span-4">
+          <div className="noisy" />
+          <img src="/images/abt4.png" alt="grid-img-4" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
