919dda79fad0ba02b77055ff3c1208320904f6cf
Vansh Suri <vanshsuri07@gmail.com>
2025-07-28 14:40:48 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { ScrollTrigger, SplitText } from "gsap/all";
 import gsap from "gsap";
-import Navbar from "./components/navbar";
+import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Cocktails from "./components/Cocktails";
 import About from "./components/About";
